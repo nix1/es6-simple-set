647576583d0bc1f4619011782fa36f6724e105a5
Paweł Sierszeń <pawelsierszen@gmail.com>
2016-07-09 23:48:04 +0200

    Implement the Set methods

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-
-export default class Set {
-
+/**
+ * A Set polyfill optimized for file size and simplicity.
+ *
+ * Notes:
+ * - Performance might be an issue if dealing with large sets,
+ *   as adding a new item will take more time.
+ * - Only a small subset of the ES6 spec is implemented,
+ *   but if you don't use weird JS stuff like NaN
+ *   you should be fine.
+ */
+export default function Set(elements = []) {
+    this.size = 0;
+    [].forEach.call(elements, (element) => this.add(element));
 }
+
+Set.prototype = [];
+
+Set.prototype.add = function(element) {
+    if (this.indexOf(element) !== -1) {
+        return this;
+    }
+    this.push(element);
+    this.size++;
+    return this;
+};
+
+Set.prototype.clear = function() {
+    this.length = this.size = 0;
+};
+
+Set.prototype.delete = function(element) {
+    const index = this.indexOf(element);
+    if (index === -1) {
+        return false;
+    }
+    this.splice(index, 1);
+    return true;
+};
+
+Set.prototype.values = Set.prototype.keys = Set.prototype.entries = function() {
+    return this.slice();
+};
+
+Set.prototype.forEach = function(callbackFn, thisArg = this) {
+    return [].forEach(val => callbackFn.call(thisArg, val, val));
+};
+
+Set.prototype.has = function(element) {
+    return this.indexOf(element) !== -1;
+};
+
+
--- test/index.js
+++ test/index.js
@@ -1,1 +1,75 @@
-import Set from '../index';
+import { expect } from 'chai';
+import S from '../index';
+
+describe("SimpleSet", function() {
+    describe("constructor", function () {
+        it("should have 0 length", function () {
+            expect(S.length).to.equal(0);
+        });
+        it("should set proper size", function () {
+            expect(new S().size).to.equal(0);
+            expect(new S([]).size).to.equal(0);
+            expect(new S(["a", "b", "c"]).size).to.equal(3);
+            expect(new S(["a", "b", "b", "c"]).size).to.equal(3);
+        });
+    });
+
+    describe("instance", function () {
+        beforeEach(function() {
+            this.s = new S(['first', 'second']);
+        });
+        it("should add properly", function () {
+            this.s.add('second');
+            this.s.add(3);
+            this.s.add('fourth');
+            expect(this.s.size).to.equal(4);
+        });
+        it("should chain", function () {
+            this.s.add('second').add('fifth').add('fifth');
+            expect(this.s.size).to.equal(3);
+        });
+        it("should check for existence", function() {
+            this.s.add('second');
+            this.s.add(3).add('fourth');
+            expect(this.s.has('2')).to.equal(false);
+            expect(this.s.has('3')).to.equal(false);
+            expect(this.s.has('first')).to.equal(true);
+        });
+        it("should return values", function() {
+            this.s.add('first');
+            this.s.add(0);
+            expect(this.s.values()).to.deep.equal(['first', 'second', 0]);
+        });
+        it("should clear", function() {
+            expect(this.s.clear()).to.equal(undefined);
+            expect(this.s.size).to.equal(0);
+            this.s.add('first');
+            expect(this.s.size).to.equal(1);
+        });
+        it("should loop (forEach)", function () {
+            this.s.forEach(function (key, value) {
+                expect(this.size).to.equal(2);
+                expect(key).to.be.oneOf(['first', 'second']);
+                expect(value).to.be.oneOf(['first', 'second']);
+            });
+        });
+        it("should bind forEach", function () {
+            this.s.forEach(function (key, value) {
+                expect(this.hello).to.equal('world');
+                expect(key).to.be.oneOf(['first', 'second']);
+                expect(value).to.be.oneOf(['first', 'second']);
+            }, {hello: 'world'});
+        });
+        it("should delete values", function () {
+            expect(this.s.values()).to.deep.equal(['first', 'second']);
+            expect(this.s.delete('kthxbai')).to.equal(false);
+            expect(this.s.values()).to.deep.equal(['first', 'second']);
+            expect(this.s.delete('first')).to.equal(true);
+            expect(this.s.values()).to.deep.equal(['second']);
+        });
+        it("should work with Array.from", function () {
+            const arr = Array.from(this.s);
+            expect(arr).to.deep.equal(this.s.values());
+        });
+    });
+});
